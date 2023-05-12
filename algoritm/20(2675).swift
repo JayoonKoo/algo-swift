1ce4f9c6abd5716663281066ceaa88d6cf53f6af
jayoonkoo <rnwkdbs12@gmail.com>
2023-05-12 23:56:11 +0900

    solve: 20

--- algoritm/20(2675).swift
+++ algoritm/20(2675).swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+/**
+
+ import Foundation
+
+ let T = Int(readLine()!)!
+
+ for _ in 0..<T {
+     let caseStr = readLine()!.split(separator: " ")
+     let roopCnt = Int(caseStr[0])!
+     
+     let answer = caseStr[1].reduce("") { partialResult, c in
+         var newResult = partialResult
+         for _ in 0..<roopCnt {
+             newResult += String(c)
+         }
+         return newResult
+     }
+     
+     print(answer)
+ }
+
+ */
