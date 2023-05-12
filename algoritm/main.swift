59c70603024a31a3e8948a14edfc0a1219374999
jayoonkoo <rnwkdbs12@gmail.com>
2023-05-13 00:02:11 +0900

    gitignore 수정

---
+++ algoritm/main.swift
@@ -0,0 +1,27 @@
+//
+//  19(11720).swift
+//  algoritm
+//
+//  Created by 구자윤 on 2023/05/12.
+//
+
+import Foundation
+
+let T = Int(readLine()!)!
+
+for _ in 0..<T {
+    let caseStr = readLine()!.split(separator: " ")
+    let roopCnt = Int(caseStr[0])!
+    
+    let answer = caseStr[1].reduce("") { partialResult, c in
+        var newResult = partialResult
+        for _ in 0..<roopCnt {
+            newResult += String(c)
+        }
+        return newResult
+    }
+    
+    print(answer)
+}
+
+
