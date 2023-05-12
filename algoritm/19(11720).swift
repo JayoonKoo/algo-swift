62c3d29dc1ad0f6d074c1db3efacd0cdc0f3158b
jayoonkoo <rnwkdbs12@gmail.com>
2023-05-12 23:55:55 +0900

    solve: 19

---
+++ algoritm/19(11720).swift
@@ -0,0 +1,22 @@
+//
+//  19(11720).swift
+//  algoritm
+//
+//  Created by 구자윤 on 2023/05/12.
+//
+
+
+import Foundation
+
+/*
+ 
+ let N = Int(readLine()!)!
+ let input = Array(readLine()!).map { cNum in
+     Int(String(cNum))!
+ }.reduce(into: 0) { partialResult, n in
+     partialResult += n
+ }
+
+ print(input)
+ 
+ */
---
+++ algoritm/20(2675).swift
@@ -0,0 +1,8 @@
+//
+//  20(2675).swift
+//  algoritm
+//
+//  Created by 구자윤 on 2023/05/12.
+//
+
+import Foundation
