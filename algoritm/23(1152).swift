c245bf3e12249d8c1f1ac75c1283f758f73f80fb
jayoonkoo <rnwkdbs12@gmail.com>
2023-05-14 14:28:49 +0900

    solve : 23(1152)

--- algoritm/23(1152).swift
+++ algoritm/23(1152).swift
@@ -1,12 +1,16 @@
 //
-//  19(11720).swift
+//  23(1152).swift
 //  algoritm
 //
-//  Created by 구자윤 on 2023/05/12.
+//  Created by 구자윤 on 2023/05/14.
 //
 
 import Foundation
 
+/**
+
 let inputs = readLine()!.split(separator: " ")
 
 print(inputs.count)
+
+ */
