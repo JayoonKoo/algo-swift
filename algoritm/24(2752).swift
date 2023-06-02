9dd28df77e3549ed824ff72c6dc22ce3e2bf620d
jayoonkoo <rnwkdbs12@gmail.com>
2023-06-02 22:53:04 +0900

    solve : 24(2752)

---
+++ algoritm/24(2752).swift
@@ -0,0 +1,19 @@
+//
+//  24(2752).swift
+//  algoritm
+//
+//  Created by 구자윤 on 2023/06/02.
+//
+
+/**
+ 
+ import Foundation
+
+ let answer = String(readLine()!).split(separator: " ").map{ Int($0)! }.sorted().reduce("") { partialResult, cur in
+     return "\(partialResult) \(cur)"
+ }
+
+
+ print(answer.trimmingCharacters(in: .whitespaces))
+ 
+ */
