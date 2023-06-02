cd83730b323ad308fc13774c91a2b9d473406c09
jayoonkoo <rnwkdbs12@gmail.com>
2023-06-02 23:12:07 +0900

    solve: 26(2751)

---
+++ algoritm/26(2751).swift
@@ -0,0 +1,26 @@
+//
+//  26(2751).swift
+//  algoritm
+//
+//  Created by 구자윤 on 2023/06/02.
+//
+
+import Foundation
+
+/**
+let N = Int(readLine()!)!
+
+var arr: [Int] = []
+
+for _ in 0..<N {
+    arr.append(Int(readLine()!)!)
+}
+
+arr.sort { $0 < $1}
+
+var answer = ""
+
+for a in arr {
+    print("\(a)")
+}
+ */
