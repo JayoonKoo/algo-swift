098d0c2e809e1f35ce8071faed725bacad7851ab
jayoonkoo <rnwkdbs12@gmail.com>
2023-06-02 23:03:17 +0900

    solve: 25(2750)

---
+++ algoritm/25(2750).swift
@@ -0,0 +1,32 @@
+//
+//  25(2750).swift
+//  algoritm
+//
+//  Created by 구자윤 on 2023/06/02.
+//
+
+import Foundation
+
+/**
+ 
+ let N = Int(readLine()!)!
+
+ var arr: [Int] = []
+
+ for _ in 0..<N {
+     arr.append(Int(readLine()!)!)
+ }
+
+ arr.sort { a, b in
+     if a < b {
+         return true
+     }
+     return false
+ }
+
+ let answer = arr.reduce("") { partialResult, cur in
+     return "\(partialResult)\(cur)\n"
+ }
+
+ print(answer.trimmingCharacters(in: .whitespaces))
+ */
