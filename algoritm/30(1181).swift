4075f08a2a2c6af44230b99f3dcdc430f0d0707c
jayoonkoo <rnwkdbs12@gmail.com>
2023-06-11 22:24:34 +0900

    solve : 30(1181)

---
+++ algoritm/30(1181).swift
@@ -0,0 +1,36 @@
+//
+//  19(11720).swift
+//  algoritm
+//
+//  Created by 구자윤 on 2023/05/12.
+//
+
+import Foundation
+
+/**
+ let N = Int(readLine()!)!
+
+ var words = Set<String>()
+ for _ in 0..<N {
+     let input = readLine()!
+     
+     words.insert(input)
+     
+ }
+
+ let answer = words.sorted { a, b in
+     if a.count < b.count {
+         return true
+     } else if a.count == b.count {
+         return a < b
+     }
+     
+     return false
+ }
+
+
+ answer.forEach { a in
+     print("\(a)")
+ }
+
+ */
