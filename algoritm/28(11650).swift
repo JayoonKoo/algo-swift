c4e2c3306f4183efb3a7129a7721bf2354a1098d
jayoonkoo <rnwkdbs12@gmail.com>
2023-06-11 21:59:12 +0900

    solve : 28(11650)

---
+++ algoritm/28(11650).swift
@@ -0,0 +1,37 @@
+//
+//  19(11720).swift
+//  algoritm
+//
+//  Created by 구자윤 on 2023/05/12.
+//
+
+import Foundation
+
+/**
+let N = Int(readLine()!)!
+
+var inputs : [(Int, Int)] = []
+for _ in 0..<N {
+    let input = readLine()!.split(separator: " ").map{ Int($0)! }
+    inputs.append((input[0], input[1]))
+}
+
+inputs.sort { a, b in
+    if a.0 < b.0 {
+        return true
+    } else if a.0 == b.0 {
+        if (a.1 < b.1) {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    return false
+}
+
+inputs.forEach { answer in
+    print("\(answer.0) \(answer.1)")
+}
+
+*/
